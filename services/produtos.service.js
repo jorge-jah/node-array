8cbda7c20dfafa96a6cf7572d9dd38d602cdf56e
jorge-jah <jorgejah14@gmail.com>
2024-02-26 21:51:44 -0300

    ajustes nas requisições e retornos

--- services/produtos.service.js
+++ services/produtos.service.js
@@ -1,13 +1,24 @@
 import { produtos } from "../db/produtos.js";
 
- let objResponse = {
+let objResponse = {
     message: '',
     data: []
- }
+}
+
+// Ao importar o módulo produto ele se comporat como  readOnly, ou seja, apenas leitura e no método remover
+// removerTodoProduto() na linha 47 ao tentar setar produtos como vazio, pegamos o erro, pegamos o erro que não
+// podemos setar em uma constante. Para realizarmos o remover desta forma, devemos declarar a variável dentro do escopo
+// do arquivo como na linha 13 e não usar mais o import.
+
+//  let produtos = []
 
 export const produtoService = {
     buscarProdutos: (req, reply) => {
-        return produtos;
+        return {
+            qtd: produtos.length,
+            data: produtos
+    }
+        // return data;
         // const text = "Screen is ready in\nPRODUTOS!"
         // reply.send(text);
     },
@@ -18,12 +29,32 @@ export const produtoService = {
         console.log(produto)
         return produto
     },
- 
+
     criarProduto: (req, reply) => {
-        // reply.send("Screen is ready POST in\n PRODUTO.");   
-        let produtoReq = req.body        
-           return produtos.push(produtoReq);
-        
+        try {
+            let idNext = produtos.length + 1;
+            const { preco } = req.body;
+
+            let produtosBD = {
+                id: idNext,
+                nome: `Produto ${idNext}`,
+                preco,
+            }
+
+            return produtos.push(produtosBD);
+
+
+            // reply.send("Screen is ready POST in\n PRODUTO."); 
+            // let produtoReq = req.body        
+            // let id = produtos.length + 1;
+
+            // req.body.id = id
+            // req.body.nome = `Produto + ${id}`
+            //    return produtos.push(produtoReq);
+
+        } catch (error) {
+            console.log(error);
+        }
     },
 
     atualizarProdutoParcial: (req, reply) => {
